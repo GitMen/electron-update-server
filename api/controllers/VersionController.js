a2d24b3e03567f7ce3beef80b957104f62e5203d
张鼎辉 <zhangdinghui@icourt.cn>
2018-10-12 19:18:36 +0800

    修改配置

--- api/controllers/VersionController.js
+++ api/controllers/VersionController.js
@@ -227,11 +227,16 @@ module.exports = {
             //   '/download/' + latestVersion.name + '/' +
             //   latestVersion.assets[0].platform + '?filetype='+latestVersion.assets[0].filetype
             // )
-
+            let filetype
+            if (latestVersion.assets[0].platform === 'osx_64') {
+              filetype = 'dmg'
+            } else {
+              filetype = 'exe'
+            }
             let updateUrl = url.resolve(
               sails.config.appUrl,
               '/download/' + latestVersion.name + '/' +
-              latestVersion.assets[0].platform + '?filetype=zip'
+              latestVersion.assets[0].platform + '?filetype=' + filetype
             )
             let responseParma = {
               url: updateUrl,
